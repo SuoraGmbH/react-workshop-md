ea85b1ffd66665a14394894662534689c45cf198
Hans-Christian Otto <c.otto@suora.com>
2022-01-14 11:03:29 +0100

    reset form on submit

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 const TimeEntryForm: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    alert("Hallo Welt");
+    console.log(inputValue);
+    setInputValue("");
+  };
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputValue(event.target.value.toUpperCase());
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <button type="submit">Submit</button>
+      <input onChange={handleChange} value={inputValue} />
+      {inputValue}
     </form>
   );
 };
