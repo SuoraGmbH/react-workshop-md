b646824560b88bcc2b1926113fb980d1e0854b6c
Hans-Christian Otto <c.otto@suora.com>
2022-01-17 17:22:32 +0100

    make props more extensible

--- src/domain/TimeEntry.ts
+++ src/domain/TimeEntry.ts
@@ -1,6 +1,15 @@
-export interface TimeEntry {
+export interface TimeEntryWithGenericDateType<TDate> {
   id: string;
   comment: string;
-  start: Date;
-  end: Date;
+  start: TDate;
+  end: TDate;
 }
+
+export type TimeEntry = TimeEntryWithGenericDateType<Date>;
+export type TimeEntryBackend = TimeEntryWithGenericDateType<string>;
+// export type TimeEntryFancyFrontendThing = TimeEntryBla<moment>;
+
+// type TimeEntryWithoutId = Omit<TimeEntry, "id">;
+// type TimeEntryTimes = Pick<TimeEntry, "start" | "end">;
+
+// const bla: TimeEntryTimes = {};
