98be279d781fd387734881fe19bb3379f1877f7d
Hans-Christian Otto <c.otto@suora.com>
2022-01-17 15:26:09 +0100

    added test for form

---
+++ src/components/TimeEntryForm.test.tsx
@@ -0,0 +1,79 @@
+import { render, screen } from "@testing-library/react";
+import TimeEntryForm from "./TimeEntryForm";
+import userEvent from "@testing-library/user-event";
+describe("<TimeEntryForm />", () => {
+  it("renders without a event handler", () => {
+    render(<TimeEntryForm />);
+  });
+
+  test("it renders a textbox", () => {
+    render(<TimeEntryForm />);
+    screen.getByRole("textbox");
+  });
+
+  test("clicking the button submits the form", () => {
+    const handleAddTimeEntryMock = jest.fn();
+
+    render(<TimeEntryForm onAddTimeEntry={handleAddTimeEntryMock} />);
+
+    const submitButton = screen.getByRole("button", {
+      name: /submit/i,
+    });
+
+    userEvent.click(submitButton);
+
+    expect(handleAddTimeEntryMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("typing into the textbox and clicking the button submits the form with the entered text", () => {
+    const handleAddTimeEntryMock = jest.fn();
+
+    render(<TimeEntryForm onAddTimeEntry={handleAddTimeEntryMock} />);
+
+    const submitButton = screen.getByRole("button", {
+      name: /submit/i,
+    });
+    const textbox = screen.getByRole("textbox");
+
+    userEvent.type(textbox, "Hallo André");
+    userEvent.click(submitButton);
+
+    expect(handleAddTimeEntryMock).toHaveBeenCalledTimes(1);
+    expect(handleAddTimeEntryMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        comment: "Hallo André",
+      })
+    );
+  });
+  test("typing into the textbox and pressing enter submits the form with the entered text", () => {
+    const handleAddTimeEntryMock = jest.fn();
+
+    render(<TimeEntryForm onAddTimeEntry={handleAddTimeEntryMock} />);
+
+    const submitButton = screen.getByRole("button", {
+      name: /submit/i,
+    });
+    const textbox = screen.getByRole("textbox");
+
+    userEvent.type(textbox, "Hallo André{enter}");
+
+    expect(handleAddTimeEntryMock).toHaveBeenCalledTimes(1);
+    expect(handleAddTimeEntryMock).toHaveBeenCalledWith(
+      expect.objectContaining({
+        comment: "Hallo André",
+      })
+    );
+  });
+
+  test("that after submitting the form is empty again", () => {
+    const handleAddTimeEntryMock = jest.fn();
+
+    render(<TimeEntryForm onAddTimeEntry={handleAddTimeEntryMock} />);
+    const textbox = screen.getByRole("textbox");
+
+    userEvent.type(textbox, "Hallo André");
+    expect(textbox).toHaveValue("Hallo André");
+    userEvent.type(textbox, "{enter}");
+    expect(textbox).toHaveValue("");
+  });
+});
