699fdbc25c166d8da78e04cff8564cbe261174d0
Hans-Christian Otto <c.otto@suora.com>
2022-01-18 15:42:49 +0100

    adjusted error type

--- src/components/GithubRepoInformationWithReactQuery.tsx
+++ src/components/GithubRepoInformationWithReactQuery.tsx
@@ -9,19 +9,21 @@ interface Props {
 const GithubRepoInformationWithReactQuery: React.FunctionComponent<Props> = ({
   repo,
 }) => {
-  const { data, isLoading, refetch, error, isRefetching } =
-    useQuery<GithubRepoData>(
-      ["repoInformation", repo],
-      ({ signal }) => {
-        return fetch(`http://localhost:3001/repos/${repo}`, { signal }).then(
-          (response) => response.json()
-        );
-      },
-      {
-        refetchInterval: 30000,
-        // refetchOnWindowFocus: false,
-      }
-    );
+  const { data, isLoading, refetch, error, isRefetching } = useQuery<
+    GithubRepoData,
+    Error
+  >(
+    ["repoInformation", repo],
+    ({ signal }) => {
+      return fetch(`http://localhost:3001/repos/${repo}`, { signal }).then(
+        (response) => response.json()
+      );
+    },
+    {
+      refetchInterval: 30000,
+      // refetchOnWindowFocus: false,
+    }
+  );
 
   if (!data) {
     return (
