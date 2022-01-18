d58d286c554ac1fa5256b5853a6873a8d56f005f
Hans-Christian Otto <c.otto@suora.com>
2022-01-18 13:02:42 +0100

    WIP

--- src/hooks/useTimeEntriesFromServerWithRefetchTrigger.ts
+++ src/hooks/useTimeEntriesFromServerWithRefetchTrigger.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { TimeEntry } from "../domain/TimeEntry";
 import { TimeEntryBackend } from "../examples/convertBackendTimeEntry";
 import useBaseUrl from "./useBaseUrl";
@@ -15,10 +15,14 @@ const useTimeEntriesFromServer = (): UseTimeEntriesFromServerReturnValue => {
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error>();
+  const [refetchTrigger, setRefetchTrigger] = useState(0);
 
-  const fetchTimeEntries = useCallback(() => {
+  useEffect(() => {
+    const abortController = new AbortController();
     setIsLoading(true);
-    fetch(baseUrl + "/timeEntries")
+    fetch(baseUrl + "/timeEntries", {
+      signal: abortController.signal,
+    })
       .then((response) => response.json())
       .then((data: TimeEntryBackend[]) => {
         const convertedTimeEntries = data.map((timeEntry): TimeEntry => {
@@ -43,43 +47,17 @@ const useTimeEntriesFromServer = (): UseTimeEntriesFromServerReturnValue => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, [baseUrl]);
+    return () => {
+      // cancel the request when unmounting the component
+      abortController.abort();
+    };
+  }, [baseUrl, refetchTrigger]);
 
-  useEffect(() => {
-    fetchTimeEntries();
-  }, [baseUrl]);
+  const refetch = () => {
+    setRefetchTrigger((prevNumber) => prevNumber + 1);
+  };
 
-  return { timeEntries, isLoading, error, refetch: fetchTimeEntries };
-};
-
-export const fetchTimeEntriesFromServerForThomas = (
-  timeEntrySetter: (timeEntries: TimeEntry[]) => void,
-  ac: AbortController
-) => {
-  const abortController = new AbortController();
-  fetch("http://localhost:3001/timeEntries", {
-    signal: abortController.signal,
-  })
-    .then((response) => response.json())
-    .then((data: TimeEntryBackend[]) => {
-      const convertedTimeEntries = data.map((timeEntry): TimeEntry => {
-        return {
-          id: timeEntry.id,
-          comment: timeEntry.comment,
-          start: new Date(timeEntry.start),
-          end: new Date(timeEntry.end),
-        };
-      });
-
-      timeEntrySetter(convertedTimeEntries);
-    })
-    .catch((error) => {
-      if (error.name === "AbortError") {
-        return;
-      }
-
-      // we should actually check if this is an Error
-    });
+  return { timeEntries, isLoading, error, refetch };
 };
 
 export default useTimeEntriesFromServer;
